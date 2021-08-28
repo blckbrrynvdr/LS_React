e739975c1c570710766d6f99112d52d01c77074b
Artur Vrublevskiy <v8a8rt@gmail.com>
2021-08-28 19:12:25 +0300

    refactor(app.jsx reducer-example.js): remove try redux changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,6 @@ import { withAuth } from "./context/auth";
 import PropTypes from "prop-types";
 
 
-
-
-import { createStore } from "redux";
- 
-const reducer = (state = { todos: [] }, action) => {
-   switch (action.type) {
-       case "ADD_TODO":
-           return { todos: [...state.todos, action.payload] };
-       default:
-           return state;
-   }
-};
- 
-const store = createStore(reducer);
- 
-store.subscribe(() => {
-   console.log(store.getState());
-});
- 
-store.dispatch({type: "ADD_TODO", payload: "Помыть посуду"});
-store.dispatch({type: "ADD_TODO", payload: "Постирать одежду"});
-store.dispatch({type: "ADD_TODO", payload: "Погулять с собакой"});
-store.dispatch({type: "ADD_TODO", payload: "Выучить реакт"});
-
-
-
-
 class App extends React.Component {
 
   static propTypes = {
@@ -42,9 +15,8 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <h1>Hello World</h1>
-        {/* {!this.props.isLoggedIn && <Welcome />}
-        {this.props.isLoggedIn && <HomeWithAuth />} */}
+        {!this.props.isLoggedIn && <Welcome />}
+        {this.props.isLoggedIn && <HomeWithAuth />}
       </>
     );
   }
