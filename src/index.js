88c5fc75d89234ffab5e2f3d15f47413ab489f47
Artur Vrublevskiy <v8a8rt@gmail.com>
2021-08-29 10:17:57 +0300

    refactor(code style): configure the codestyle linters

--- src/index.js
+++ src/index.js
@@ -1,49 +1,13 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { AuthProvider } from './context/auth';
-import { theme } from "loft-taxi-mui-theme"; 
-import { MuiThemeProvider } from "@material-ui/core/styles";
-
-
-
-import { createStore } from "redux";
-import reducer, { ADD_TODO, SET_VISIBILITY_FILTER } from "./reducer-example";
+import SimpleRouter from './SimpleRouter';
+import { BrowserRouter } from 'react-router-dom';
  
-const addTodo = text => ({
-   type: ADD_TODO,
-   text
-});
- 
-const setVisibilityFilter = filter => ({
-   type: SET_VISIBILITY_FILTER,
-   filter
-});
- 
-const store = createStore(reducer);
- 
-store.subscribe(() => {
-   console.log(store.getState());
-});
- 
-store.dispatch(addTodo("Помыть посуду"));
-store.dispatch(addTodo("Постирать одежду"));
-store.dispatch(addTodo("Погулять с собакой"));
-store.dispatch(addTodo("Выучить реакт"));
- 
-store.dispatch(setVisibilityFilter("ONLY_COMPLETED"));
-
-
-
-
-ReactDOM.render( 
-  <React.StrictMode>
-    <MuiThemeProvider theme={theme}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </MuiThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(
+ <BrowserRouter>
+   <SimpleRouter />
+ </BrowserRouter>,
+ document.getElementById('root'),
 );
